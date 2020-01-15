0cc005a2dfa3075aa8bc64263d01b04b84566334
Stefan Böhringer <github@s-boehringer.org>
2020-01-15 12:05:09 +0100

    Commit for onward development of version 0.5-0

--- R/Rmeta.R
+++ R/Rmeta.R
@@ -316,6 +316,7 @@ thawCall = function(
 	envir = .GlobalEnv) {
 
 	load(freeze_file, envir = envir);
+	# <!> untested change [addition of line] 15.1.2020
 	callSpecification = get('callSpecification');
 	r = with(callSpecification, {
 		for (library in freeze_control$libraries) {
