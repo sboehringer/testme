0476fcf9c91eab2135ec592b2918972ad6315a6e
Stefan Böhringer <github@s-boehringer.org>
2020-01-17 11:13:52 +0100

    Commit for onward development of version 0.6-0

--- R/RtestingHelpers.R
+++ R/RtestingHelpers.R
@@ -151,6 +151,8 @@ testmeFileSingle = function(file, expectationsFolder, useGit, print = F, logger 
 	if (print) testmePrintReport(rTests);
 	return(rTests);
 }
+
+testmeFileV = Vectorize(testmeFileSingle, 'file');
 #' Run all tests defined in a collection of R-scripts
 #'
 #' Collect testing functions from files and run tests
@@ -164,10 +166,11 @@ testmeFileSingle = function(file, expectationsFolder, useGit, print = F, logger 
 #' @param useGit logical to inidicate whether vivifications are to be commited by a call to `git`
 #' @param print logicial to indicate whether a report is to be printed
 #' @param logger function that is used to print results, can be used to redirect output
-#' @param character artifact created by roxygen/Vectorize
 #' @return returns a list for each test executed that contains testing status and number of subtests run
 #' @export testmeFile
-testmeFile = Vectorize(testmeFileSingle, 'file');
+testmeFile = function(file, expectationsFolder, useGit, print = F, logger = LogAt1)
+	testmeFileV(file, expectationsFolder, useGit, print, logger)
+# above necessary due to roxygen not being able to handle Vectorize properly
 
 #' Run all tests defined in a folder
 #'
