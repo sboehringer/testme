eb5da3a90d6c61c601d2ae02eee4c8e389547683
Stefan Böhringer <github@s-boehringer.org>
2020-02-28 10:42:52 +0100

    Commit for onward development of version 1.2-0

--- tests/testme_run.R
+++
@@ -1,4 +0,0 @@
-# This runs tests `testme`
-#testmeEnvInit('RtestsExpectations', logger = print);
-library('testme');
-print(testmeFileSingle('testme/testme.R', 'testme/RtestsExpectations', useGit = FALSE, logger = print));
